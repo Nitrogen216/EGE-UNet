f1378061b3a90b8ea5bf7422a3c3ae1fb2f09160
Nitrogen216 <90951363+Nitrogen216@users.noreply.github.com>
2023-09-24 19:41:51 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -250,7 +250,36 @@ def save_imgs(img, msk, msk_pred, i, save_path, datasets, threshold=0.5, test_da
     plt.savefig(save_path + str(i) +'.png')
     plt.close()
     
+def save_imgs_name(img, msk, msk_pred, img_name, save_path, datasets, threshold=0.5, test_data_name=None):
+    img = img.squeeze(0).permute(1,2,0).detach().cpu().numpy()
+    img = img / 255. if img.max() > 1.1 else img
+    if datasets == 'retinal':
+        msk = np.squeeze(msk, axis=0)
+        msk_pred = np.squeeze(msk_pred, axis=0)
+    else:
+        msk = np.where(np.squeeze(msk, axis=0) > 0.5, 1, 0)
+        msk_pred = np.where(np.squeeze(msk_pred, axis=0) > threshold, 1, 0)
 
+    plt.figure(figsize=(7,15))
+
+    plt.subplot(3,1,1)
+    plt.imshow(img)
+    plt.axis('off')
+
+    plt.subplot(3,1,2)
+    plt.imshow(msk, cmap= 'gray')
+    plt.axis('off')
+
+    plt.subplot(3,1,3)
+    plt.imshow(msk_pred, cmap = 'gray')
+    plt.axis('off')
+
+    if test_data_name is not None:
+        save_path = save_path + test_data_name + '_'
+
+    img_name_str = img_name[0]
+    plt.savefig(save_path + img_name_str +'.png')
+    plt.close()
 
 class BCELoss(nn.Module):
     def __init__(self):
